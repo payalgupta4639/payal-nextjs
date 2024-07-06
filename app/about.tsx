357ee357653f3b306535d776d9c4cd8d1e2c14fa
payalgupta4639 <payalgupta4639@gmail.com>
2024-07-06 15:45:42 +0530

    about page

---
+++ app/about.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Us 2</h1>
+      <p>Welcome to the about page.</p>
+    </div>
+  );
+};
+
+export default About;
