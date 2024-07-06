8b239745448e53740d091cebb72aa9e65f18bc5e
payalgupta4639 <payalgupta4639@gmail.com>
2024-07-06 15:33:39 +0530

    add new route

---
+++ app/pages/questions.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Questions = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to the about page.</p>
+    </div>
+  );
+};
+
+export default About;
