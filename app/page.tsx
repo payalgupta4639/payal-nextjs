8b149595d9c36268e04066f09a71566d6b266099
payalgupta4639 <payalgupta4639@gmail.com>
2024-07-07 21:44:52 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 w-full max-w-5xl flex flex-col items-center justify-between font-mono text-sm lg:flex-row lg:items-stretch">
         <h1 id="familiarization-exercise">Familiarization Exercise</h1>
         
         <ol>
