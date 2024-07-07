1cca73fb77a4018c2543f6e96bd273270f3a84c8
payalgupta4639 <payalgupta4639@gmail.com>
2024-07-07 23:54:57 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -195,9 +195,11 @@ export default function Home() {
             <ul>
               <li>1. Open your Nginx configuration file for editing.</li>
               <li>2. Add the following 'location' block inside your server block:</li>
-              <code>
-                
-              </code>
+              <pre>
+                { ... location /blog 
+                     { return 301 https://example.com; }
+                ... }
+              </pre>
               <li>3. Save the configuration file and reload Nginx for changes to take effect.</li>
             </ul>
             <p>
