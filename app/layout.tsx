e01098550accb1e8519af42d40d79baaa9b959e9
payalgupta4639 <payalgupta4639@gmail.com>
2024-07-07 21:36:06 +0530

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,11 +16,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <meta name="robots" content="noindex, nofollow"/>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Familiarization Exercise</title>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
